bfbf5b48e8cc0693b23054d20d6d618636292bcd
ADoyle1001 <adoyle205@caledonian.ac.uk>
2020-05-06 23:29:23 +0100

    push final commit

--- index.js
+++ index.js
@@ -1,32 +1,31 @@
-var express = require ("express"),
+var express = require("express"),
+session = require('express-session'),
 path = require("path"),
 mustache = require('mustache-express'),
 bodyParser = require('body-parser'),
-controller = require('./controllers/routes.js');
+controller = require('./controllers/routes.js'),
+auth = require('./auth/auth');
 
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
-
 app.engine('mustache', mustache());
+
 app.set('view engine', 'mustache');
 
-//var staticPath = path.resolve(__dirname, "/static");
-app.use(express.static('static'));
+var staticPath = path.resolve(__dirname, "../static");
+app.use(express.static(staticPath));
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use('/', controller);
+app.use(session({ secret: 'dont tell anyone', resave: false, saveUninitialized: false }));
 
-app.use(function (req, res) {
-    res.status(404);
-    res.type('text/plain');
-    res.send("404 - Not found.");
-});
+// initialize authentication with passport
+auth.init(app); 
+
+app.use('/', controller);
 
 app.listen(app.get('port'), function () {
     console.log('server started, ctl^c to quit');
 })
-
-
 
--- models/courseworks.js
+++ models/courseworks.js
@@ -106,6 +106,9 @@ init() {
         });
     }
 }
+
+
+
 module.exports = DAO;
 
 
